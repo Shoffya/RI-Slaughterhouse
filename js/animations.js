b3ecdb9b42f784d464c29d662a0b7d06d62b774a
Soffya <inguinha.fenandes@gmail.com>
2022-11-29 17:49:53 -0300

    bugfix

--- js/animations.js
+++ js/animations.js
@@ -8,27 +8,23 @@ function onOff() {
         about.style.display = "none";
         title2.style.display = "none";
         title.style.display = "flex";
+        video.play();
     }
     if (nav2.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         title.style.display = "none";
         title2.style.display = "none";
         about.style.display = "flex";
+        video.play();
     }
     if (nav3.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
         title.style.display = "none";
         about.style.display = "none";
         title2.style.display = "flex";
-    }
-
-}
-
-function playVideo() {
-    if (video.pause() == true) {
         video.play();
     }
+
 }
 
 setInterval(() => {
     onOff();
-    playVideo();
 }, 500);
