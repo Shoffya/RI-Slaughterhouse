12e8ec4e1867f5b4819c28d8dfd6d0f271e165d1
Soffya <inguinha.fenandes@gmail.com>
2022-11-29 18:09:56 -0300

    bugfix

--- js/animations.js
+++ js/animations.js
@@ -4,26 +4,26 @@ var about = document.getElementById('about');
 var title2 = document.getElementById('title2');
 
 function onOff() {
-    if (nav1.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/" || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
+    if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/" || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
         about.style.display = "none";
         title2.style.display = "none";
         title.style.display = "flex";
     }
-    if (nav2.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
+    else if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         title.style.display = "none";
         title2.style.display = "none";
         about.style.display = "flex";
     }
-    if (nav3.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
+    else if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
         title.style.display = "none";
         about.style.display = "none";
         title2.style.display = "flex";
     }
+    video.play();
 
 }
 
 setInterval(() => {
     video.play();
     onOff();
-
 }, 500);
