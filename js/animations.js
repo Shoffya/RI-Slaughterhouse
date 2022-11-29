f2a5ac5860398bb3fedcef6b989e5dbddb939de9
Soffya <inguinha.fenandes@gmail.com>
2022-11-29 16:10:47 -0300

    Modifying the navbar links

--- js/animations.js
+++ js/animations.js
@@ -2,15 +2,15 @@ var title = document.getElementById('title');
 var about = document.getElementById('about');
 
 function onOff() {
-    if (nav1.classList.contains('active') === true) {
+    if (nav1.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#content" || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/") {
         title.style.display = "flex";
         about.style.display = "none";
     }
-    if (nav2.classList.contains('active') === true) {
+    if (nav2.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         title.style.display = "none";
         about.style.display = "flex";
     }
-    if (nav3.classList.contains('active') === true) {
+    if (nav3.classList.contains('active') === true || window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         title.style.display = "none";
         about.style.display = "none";
     }
@@ -18,4 +18,5 @@ function onOff() {
 
 setInterval(() => {
     onOff();
+    console.log(window.location.href)
 }, 500);
