893889461c349c2aedf85c49a20296f8d4188ad9
Soffya <inguinha.fenandes@gmail.com>
2022-12-01 09:58:34 -0300

    bugfix

--- js/animations.js
+++ js/animations.js
@@ -3,26 +3,26 @@ var title = document.getElementById('title');
 var about = document.getElementById('about');
 var title2 = document.getElementById('title2');
 
-function onOff() {
-    if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/") {
-        about.style.display = "none";
-        title2.style.display = "none";
-        title.style.display = "flex";
-    }
-    if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
-        title.style.display = "none";
-        title2.style.display = "none";
-        about.style.display = "flex";
-    }
-    if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
-        title.style.display = "none";
-        about.style.display = "none";
-        title2.style.display = "flex";
-    }
-    video.play();
+// function onOff() {
+//     if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/") {
+//         about.style.display = "none";
+//         title2.style.display = "none";
+//         title.style.display = "flex";
+//     }
+//     if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
+//         title.style.display = "none";
+//         title2.style.display = "none";
+//         about.style.display = "flex";
+//     }
+//     if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
+//         title.style.display = "none";
+//         about.style.display = "none";
+//         title2.style.display = "flex";
+//     }
+//     video.play();
 
-}
+// }
 
-setInterval(() => {
-    onOff();
-}, 500);
+// setInterval(() => {
+//     onOff();
+// }, 500);
