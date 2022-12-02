0660c55ad01ce51588535f16efc86fd6df593851
Soffya <inguinha.fenandes@gmail.com>
2022-12-02 20:07:20 -0300

    bugfix

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -3,42 +3,40 @@ var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
 
 
-// function navbarActive1(id) {
-//     if (nav1.classList.contains('active') === false) {
-//         if (nav2.classList.contains('active') || nav3.classList.contains('active')) {
-//             nav2.classList.remove('active');
-//             nav3.classList.remove('active');
-//             nav1.classList.add('active');
-//         }
-//     } else {
-//         nav1.classList.add('active');
-//     }
-// }
+function navbarActive1(id) {
+    if (nav1.classList.contains('active') === false) {
+        if (nav2.classList.contains('active') || nav3.classList.contains('active')) {
+            nav2.classList.remove('active');
+            nav3.classList.remove('active');
+            nav1.classList.add('active');
+        }
+    } else {
+        nav1.classList.add('active');
+    }
+}
 
-// function navbarActive2(id) {
-//     if (nav2.classList.contains('active') === false) {
-//         if (nav1.classList.contains('active') || nav3.classList.contains('active')) {
-//             nav1.classList.remove('active');
-//             nav3.classList.remove('active');
-//             nav2.classList.add('active');
-//         }
+function navbarActive2(id) {
+    if (nav2.classList.contains('active') === false) {
+        if (nav1.classList.contains('active') || nav3.classList.contains('active')) {
+            nav1.classList.remove('active');
+            nav3.classList.remove('active');
+            nav2.classList.add('active');
+        }
+    }
+    else {
+        nav2.classList.add('active');
+    }
+}
 
-//     }
-//     else {
-//         nav2.classList.add('active');
-//     }
-// }
-
-// function navbarActive3(id) {
-//     if (nav3.classList.contains('active') === false) {
-//         if (nav1.classList.contains('active') || nav2.classList.contains('active')) {
-//             nav1.classList.remove('active');
-//             nav2.classList.remove('active');
-//             nav3.classList.add('active');
-//         }
-
-//     }
-//     else {
-//         nav3.classList.add('active');
-//     }
-// }
+function navbarActive3(id) {
+    if (nav3.classList.contains('active') === false) {
+        if (nav1.classList.contains('active') || nav2.classList.contains('active')) {
+            nav1.classList.remove('active');
+            nav2.classList.remove('active');
+            nav3.classList.add('active');
+        }
+    }
+    else {
+        nav3.classList.add('active');
+    }
+}
