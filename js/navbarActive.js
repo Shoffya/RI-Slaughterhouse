ade57faee1a15db6d06a25ad336eb9b77e24dd76
Soffya <inguinha.fenandes@gmail.com>
2022-12-03 15:13:46 -0300

    test

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -2,9 +2,7 @@ var video = document.getElementById('background');
 var title = document.getElementById('title');
 var about = document.getElementById('about');
 var title2 = document.getElementById('title2');
-var nav1 = document.getElementById('option1');
-var nav2 = document.getElementById('option2');
-var nav3 = document.getElementById('option3');
+var navBarList = document.getElementById('navBarList');
 
 // setInterval(() => {
 // if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/") {
@@ -54,30 +52,32 @@ setInterval(() => {
         nav3.classList.remove('active');
         nav1.classList.add('active');
     }
-    nav1.addEventListener('click', () => {
-        nav2.classList.remove('active');
-        nav3.classList.remove('active');
-        nav1.classList.add('active');
-        title.style.display = "flex";
-        about.style.display = "none";
-        title2.style.display = "none";
-    });
-
-    nav2.addEventListener('click', () => {
-        nav1.classList.remove('active');
-        nav3.classList.remove('active');
-        nav2.classList.add('active');
-        about.style.display = "flex";
-        title.style.display = "none";
-        title2.style.display = "none";
-    });
-
-    nav3.addEventListener('click', () => {
-        nav1.classList.remove('active');
-        nav2.classList.remove('active');
-        nav3.classList.add('active');
-        title2.style.display = "flex";
-        title.style.display = "none";
-        about.style.display = "none";
-    });
+    else {
+        navBarList.addEventListener('click', (e) => {
+            if (e.target.id == "option1") {
+                nav2.classList.remove('active');
+                nav3.classList.remove('active');
+                nav1.classList.add('active');
+                title.style.display = "flex";
+                about.style.display = "none";
+                title2.style.display = "none";
+            }
+            else if (e.target.id == "option2") {
+                nav1.classList.remove('active');
+                nav3.classList.remove('active');
+                nav2.classList.add('active');
+                about.style.display = "flex";
+                title.style.display = "none";
+                title2.style.display = "none";
+            }
+            else if (e.target.id == "option3") {
+                nav1.classList.remove('active');
+                nav2.classList.remove('active');
+                nav3.classList.add('active');
+                title2.style.display = "flex";
+                title.style.display = "none";
+                about.style.display = "none";
+            }
+        });
+    }
 }, 500);
