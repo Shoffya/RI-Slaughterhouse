d07ceb62a23183a0f6c3956cef37f391a5863d37
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 13:51:22 -0300

    testing the links

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -5,82 +5,70 @@ var title2 = document.getElementById('title2');
 var nav1 = document.getElementById('option1');
 var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
+var navB;
 
-// nav1.addEventListener('click', () => {
-//     if (nav1.classList.contains('active') === false) {
-//         if (nav2.classList.contains('active') || nav3.classList.contains('active')) {
-//             nav2.classList.remove('active')
-//             nav3.classList.remove('active')
-//             nav1.classList.add('active')
-//             title.style.display = "flex";
-//             about.style.display = "none";
-//             title2.style.display = "none";
-//         }
-//     } else {
-//         nav1.classList.add('active')
-//     }
-// });
-
-// nav2.addEventListener('click', () => {
-//     if (nav2.classList.contains('active') === false) {
-//         if (nav1.classList.contains('active') || nav3.classList.contains('active')) {
-//             nav1.classList.remove('active')
-//             nav3.classList.remove('active')
-//             nav2.classList.add('active')
-//             about.style.display = "flex";
-//             title.style.display = "none";
-//             title2.style.display = "none";
-//         }
-//     } else {
-//         nav2.classList.add('active')
-//     }
-// });
-
-// nav3.addEventListener('click', () => {
-//     if (nav3.classList.contains('active') === false) {
-//         if (nav1.classList.contains('active') || nav2.classList.contains('active')) {
-//             nav1.classList.remove('active')
-//             nav2.classList.remove('active')
-//             nav3.classList.add('active')
-//             title2.style.display = "flex";
-//             title.style.display = "none";
-//             about.style.display = "none";
-//         }
-//     } else {
-//         nav3.classList.add('active')
-//     }
-// });
-
-setInterval(() => {
-
-    if (window.location.href == "https://shoffya.github.io/RI-Slaughterhouse/") {
-        window.location.href = "https://shoffya.github.io/RI-Slaughterhouse/#content";
-    }
-    if (window.location.href == nav1.href) {
-        console.log('/ ou content');
-        nav2.classList.remove('active')
-        nav3.classList.remove('active')
+nav1.addEventListener('click', () => {
+    if (nav1.classList.contains('active') === false) {
+        if (nav2.classList.contains('active') || nav3.classList.contains('active')) {
+            nav2.classList.remove('active')
+            nav3.classList.remove('active')
+            nav1.classList.add('active')
+            title.style.display = "flex";
+            about.style.display = "none";
+            title2.style.display = "none";
+        }
+    } else {
         nav1.classList.add('active')
-        about.style.display = "none";
-        title2.style.display = "none";
         title.style.display = "flex";
+        about.style.display = "none";
+        title2.style.display = "none";
     }
-    else if (window.location.href == nav2.href) {
-        console.log('about');
-        nav1.classList.remove('active')
-        nav3.classList.remove('active')
+
+    navB = nav1;
+
+    console.log(navB);
+});
+
+nav2.addEventListener('click', () => {
+    if (nav2.classList.contains('active') === false) {
+        if (nav1.classList.contains('active') || nav3.classList.contains('active')) {
+            nav1.classList.remove('active')
+            nav3.classList.remove('active')
+            nav2.classList.add('active')
+            about.style.display = "flex";
+            title.style.display = "none";
+            title2.style.display = "none";
+        }
+    } else {
         nav2.classList.add('active')
+        about.style.display = "flex";
         title.style.display = "none";
         title2.style.display = "none";
-        about.style.display = "flex";
     }
-    else if (window.location.href == nav3.href) {
-        console.log('title2');
-        nav1.classList.remove('active')
-        nav2.classList.remove('active')
+
+    navB = nav2;
+
+    console.log(navB);
+});
+
+nav3.addEventListener('click', () => {
+    if (nav3.classList.contains('active') === false) {
+        if (nav1.classList.contains('active') || nav2.classList.contains('active')) {
+            nav1.classList.remove('active')
+            nav2.classList.remove('active')
+            nav3.classList.add('active')
+            title2.style.display = "flex";
+            title.style.display = "none";
+            about.style.display = "none";
+        }
+    } else {
         nav3.classList.add('active')
+        title2.style.display = "flex";
         title.style.display = "none";
         about.style.display = "none";
-        title2.style.display = "flex";
     }
-}, 500);
+
+    navB = nav3;
+
+    console.log(navB);
+});
