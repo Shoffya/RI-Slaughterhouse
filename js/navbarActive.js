f83e35a4fa9ef7c19eca9321488d2046d97ca497
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 17:02:38 -0300

    changing names

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -1,5 +1,5 @@
 var video = document.getElementById('background');
-var title = document.getElementById('title');
+var homeStart = document.getElementById('homeStart');
 var about = document.getElementById('about');
 var recoverPassword = document.getElementById('recoverPassword');
 var nav1 = document.getElementById('option1');
@@ -7,11 +7,11 @@ var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
 
 setInterval(() => {
-    if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
+    if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#home") {
         nav2.classList.remove('active');
         nav3.classList.remove('active');
         nav1.classList.add('active');
-        title.style.display = "flex";
+        homeStart.style.display = "flex";
         about.style.display = "none";
         recoverPassword.style.display = "none";
     }
@@ -20,7 +20,7 @@ setInterval(() => {
         nav3.classList.remove('active');
         nav2.classList.add('active');
         about.style.display = "flex";
-        title.style.display = "none";
+        homeStart.style.display = "none";
         recoverPassword.style.display = "none";
     }
     else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#recoverPassword") {
@@ -28,14 +28,14 @@ setInterval(() => {
         nav2.classList.remove('active');
         nav3.classList.add('active');
         recoverPassword.style.display = "flex";
-        title.style.display = "none";
+        homeStart.style.display = "none";
         about.style.display = "none";
     }
     else {
         nav2.classList.remove('active');
         nav3.classList.remove('active');
         nav1.classList.add('active');
-        title.style.display = "flex";
+        homeStart.style.display = "flex";
         about.style.display = "none";
         recoverPassword.style.display = "none";
     }
