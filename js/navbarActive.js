ad79675283975654eba283553a314fbac0c9648e
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 15:38:50 -0300

    test

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -6,33 +6,6 @@ var nav1 = document.getElementById('option1');
 var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
 
-// nav1.addEventListener('click', () => {
-//     nav2.classList.remove('active');
-//     nav3.classList.remove('active');
-//     nav1.classList.add('active');
-//     title.style.display = "flex";
-//     about.style.display = "none";
-//     title2.style.display = "none";
-// });
-
-// nav2.addEventListener('click', () => {
-//     nav1.classList.remove('active');
-//     nav3.classList.remove('active');
-//     nav2.classList.add('active');
-//     about.style.display = "flex";
-//     title.style.display = "none";
-//     title2.style.display = "none";
-// });
-
-// nav3.addEventListener('click', () => {
-//     nav1.classList.remove('active');
-//     nav2.classList.remove('active');
-//     nav3.classList.add('active');
-//     title2.style.display = "flex";
-//     title.style.display = "none";
-//     about.style.display = "none";
-// });
-
 setInterval(() => {
     if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
         nav2.classList.remove('active');
@@ -41,8 +14,6 @@ setInterval(() => {
         title.style.display = "flex";
         about.style.display = "none";
         title2.style.display = "none";
-
-        console.log("content");
     }
     else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         nav1.classList.remove('active');
@@ -51,8 +22,6 @@ setInterval(() => {
         about.style.display = "flex";
         title.style.display = "none";
         title2.style.display = "none";
-
-        console.log("about");
     }
     else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
         nav1.classList.remove('active');
@@ -61,11 +30,8 @@ setInterval(() => {
         title2.style.display = "flex";
         title.style.display = "none";
         about.style.display = "none";
-
-        console.log("title2");
     }
     else {
-        console.log("/");
         nav2.classList.remove('active');
         nav3.classList.remove('active');
         nav1.classList.add('active');
