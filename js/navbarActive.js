80523df349919df10a3f9503677ff8fd3be8e64d
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 16:32:59 -0300

    buffix

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -1,41 +1,42 @@
-var home = document.getElementById('home');
+var video = document.getElementById('background');
+var title = document.getElementById('title');
 var about = document.getElementById('about');
-var recovery = document.getElementById('recovery');
+var title2 = document.getElementById('title2');
 var nav1 = document.getElementById('option1');
 var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
 
 setInterval(() => {
-    if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#home") {
+    if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
         nav2.classList.remove('active');
         nav3.classList.remove('active');
         nav1.classList.add('active');
-        home.style.display = "flex";
+        title.style.display = "flex";
         about.style.display = "none";
-        recovery.style.display = "none";
+        title2.style.display = "none";
     }
     else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         nav1.classList.remove('active');
         nav3.classList.remove('active');
         nav2.classList.add('active');
         about.style.display = "flex";
-        home.style.display = "none";
-        recovery.style.display = "none";
+        title.style.display = "none";
+        title2.style.display = "none";
     }
-    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#recovery") {
+    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
         nav1.classList.remove('active');
         nav2.classList.remove('active');
         nav3.classList.add('active');
-        recovery.style.display = "flex";
-        home.style.display = "none";
+        title2.style.display = "flex";
+        title.style.display = "none";
         about.style.display = "none";
     }
     else {
         nav2.classList.remove('active');
         nav3.classList.remove('active');
         nav1.classList.add('active');
-        home.style.display = "flex";
+        title.style.display = "flex";
         about.style.display = "none";
-        recovery.style.display = "none";
+        title2.style.display = "none";
     }
 }, 500);
