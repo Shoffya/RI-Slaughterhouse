95ddc25c7c566f8b85357e6cf30f256e8ba29e4c
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 14:51:59 -0300

    testing the links

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -8,67 +8,64 @@ var nav3 = document.getElementById('option3');
 var navB;
 
 nav1.addEventListener('click', () => {
-    if (nav1.classList.contains('active') === false) {
-        if (nav2.classList.contains('active') || nav3.classList.contains('active')) {
-            nav2.classList.remove('active')
-            nav3.classList.remove('active')
-            nav1.classList.add('active')
-            title.style.display = "flex";
-            about.style.display = "none";
-            title2.style.display = "none";
-        }
-    } else {
-        nav1.classList.add('active')
-        title.style.display = "flex";
-        about.style.display = "none";
-        title2.style.display = "none";
-    }
-
-    navB = nav1;
-
-    console.log(navB);
+    nav2.classList.remove('active');
+    nav3.classList.remove('active');
+    nav1.classList.add('active');
+    title.style.display = "flex";
+    about.style.display = "none";
+    title2.style.display = "none";
 });
 
 nav2.addEventListener('click', () => {
-    if (nav2.classList.contains('active') === false) {
-        if (nav1.classList.contains('active') || nav3.classList.contains('active')) {
-            nav1.classList.remove('active')
-            nav3.classList.remove('active')
-            nav2.classList.add('active')
-            about.style.display = "flex";
-            title.style.display = "none";
-            title2.style.display = "none";
-        }
-    } else {
-        nav2.classList.add('active')
-        about.style.display = "flex";
-        title.style.display = "none";
-        title2.style.display = "none";
-    }
-
-    navB = nav2;
-
-    console.log(navB);
+    nav1.classList.remove('active');
+    nav3.classList.remove('active');
+    nav2.classList.add('active');
+    about.style.display = "flex";
+    title.style.display = "none";
+    title2.style.display = "none";
 });
 
 nav3.addEventListener('click', () => {
-    if (nav3.classList.contains('active') === false) {
-        if (nav1.classList.contains('active') || nav2.classList.contains('active')) {
-            nav1.classList.remove('active')
-            nav2.classList.remove('active')
-            nav3.classList.add('active')
-            title2.style.display = "flex";
-            title.style.display = "none";
-            about.style.display = "none";
-        }
-    } else {
-        nav3.classList.add('active')
+    nav1.classList.remove('active');
+    nav2.classList.remove('active');
+    nav3.classList.add('active');
+    title2.style.display = "flex";
+    title.style.display = "none";
+    about.style.display = "none";
+});
+
+setInterval(() => {
+    if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
+        nav2.classList.remove('active');
+        nav3.classList.remove('active');
+        nav1.classList.add('active');
+        title.style.display = "flex";
+        about.style.display = "none";
+        title2.style.display = "none";
+
+        console.log("content");
+    }
+    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
+        nav1.classList.remove('active');
+        nav3.classList.remove('active');
+        nav2.classList.add('active');
+        about.style.display = "flex";
+        title.style.display = "none";
+        title2.style.display = "none";
+
+        console.log("about");
+    }
+    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#title2") {
+        nav1.classList.remove('active');
+        nav2.classList.remove('active');
+        nav3.classList.add('active');
         title2.style.display = "flex";
         title.style.display = "none";
         about.style.display = "none";
+
+        console.log("title2");
     }
-
-    navB = nav3;
-
-    console.log(navB);
-});
+    else {
+        console.log("/");
+    }
+}, 500);
