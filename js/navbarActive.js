3ebb868133bfbdcbe506e18dd36e110b0904b0f6
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 15:02:12 -0300

    testing the links

--- js/navbarActive.js
+++ js/navbarActive.js
@@ -5,34 +5,33 @@ var title2 = document.getElementById('title2');
 var nav1 = document.getElementById('option1');
 var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
-var navB;
 
-nav1.addEventListener('click', () => {
-    nav2.classList.remove('active');
-    nav3.classList.remove('active');
-    nav1.classList.add('active');
-    title.style.display = "flex";
-    about.style.display = "none";
-    title2.style.display = "none";
-});
+// nav1.addEventListener('click', () => {
+//     nav2.classList.remove('active');
+//     nav3.classList.remove('active');
+//     nav1.classList.add('active');
+//     title.style.display = "flex";
+//     about.style.display = "none";
+//     title2.style.display = "none";
+// });
 
-nav2.addEventListener('click', () => {
-    nav1.classList.remove('active');
-    nav3.classList.remove('active');
-    nav2.classList.add('active');
-    about.style.display = "flex";
-    title.style.display = "none";
-    title2.style.display = "none";
-});
+// nav2.addEventListener('click', () => {
+//     nav1.classList.remove('active');
+//     nav3.classList.remove('active');
+//     nav2.classList.add('active');
+//     about.style.display = "flex";
+//     title.style.display = "none";
+//     title2.style.display = "none";
+// });
 
-nav3.addEventListener('click', () => {
-    nav1.classList.remove('active');
-    nav2.classList.remove('active');
-    nav3.classList.add('active');
-    title2.style.display = "flex";
-    title.style.display = "none";
-    about.style.display = "none";
-});
+// nav3.addEventListener('click', () => {
+//     nav1.classList.remove('active');
+//     nav2.classList.remove('active');
+//     nav3.classList.add('active');
+//     title2.style.display = "flex";
+//     title.style.display = "none";
+//     about.style.display = "none";
+// });
 
 setInterval(() => {
     if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#content") {
