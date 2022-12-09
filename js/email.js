b302f70c9b2bd1d0b606a5dcac0cdb85e3c7edbb
Soffya <inguinha.fenandes@gmail.com>
2022-12-08 22:29:07 -0300

    testing email link

--- js/email.js
+++
@@ -1,8 +0,0 @@
-const email = document.getElementById("email");
-const button = document.getElementById("button");
-
-button.addEventListener('click', () => {
-    // "mailto:turpyun.rigames@gmail.com?subject=Password Recovery 🕵🏼‍♂️&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente"
-
-    window.open("mailto:email@provedor.com.br?subject=Assunto do email&cc=copia@provedor.com.br&bcc=copiaoculta@provedor.com.br&body=Conteúdo do email que será preenchido automaticamente.value");
-});
