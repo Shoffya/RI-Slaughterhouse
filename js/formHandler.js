e81010bc093e3f32892edbd43d2cc56550491a11
Soffya <inguinha.fenandes@gmail.com>
2022-12-12 15:46:17 -0300

    Modifying the support tab layout

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,9 +1,8 @@
-let btn = document.querySelector("#recovery");
+let btn = document.querySelector("#message");
 const valid = new RegExp("[a-zA-z]+[0-9]*.@(hotmail|gmail).com")
 
 btn.addEventListener('click', () => {
-    var email = document.getElementById("formEmail").elements.namedItem("email").value;
-    var message = document.getElementById("formEmail").elements.namedItem("email").value;
+    var message = document.getElementById("formEmail").elements.namedItem("message").value;
 
     if (valid.test(email)) {
     }
