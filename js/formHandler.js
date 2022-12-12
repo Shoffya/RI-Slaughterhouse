1a222801d188e900b488499ced9fbf253c97b584
Soffya <inguinha.fenandes@gmail.com>
2022-12-12 14:43:58 -0300

    Modifying the support tab layout

--- js/formHandler.js
+++ js/formHandler.js
@@ -3,19 +3,8 @@ const valid = new RegExp("[a-zA-z]+[0-9]*.@(hotmail|gmail).com")
 
 btn.addEventListener('click', () => {
     var email = document.getElementById("formEmail").elements.namedItem("email").value;
+    var message = document.getElementById("formEmail").elements.namedItem("email").value;
 
     if (valid.test(email)) {
-        $.ajax({
-            method: 'POST',
-            url: 'https://formsubmit.co/ajax/' + email,
-            dataType: 'json',
-            accepts: 'application/json',
-            data: {
-                name: "Password recovery",
-                message: "Hello! Your new password is test"
-            },
-            success: (data) => console.log(data),
-            error: (err) => console.log(err)
-        });
     }
 });
--- js/navbarActive.js
+++ js/navbarActive.js
@@ -1,7 +1,7 @@
 var video = document.getElementById('background');
 var homeStart = document.getElementById('homeStart');
 var about = document.getElementById('about');
-var recoverPassword = document.getElementById('recoverPassword');
+var supportService = document.getElementById('supportService');
 var nav1 = document.getElementById('option1');
 var nav2 = document.getElementById('option2');
 var nav3 = document.getElementById('option3');
@@ -13,7 +13,7 @@ setInterval(() => {
         nav1.classList.add('active');
         homeStart.style.display = "flex";
         about.style.display = "none";
-        recoverPassword.style.display = "none";
+        supportService.style.display = "none";
     }
     else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#about") {
         nav1.classList.remove('active');
@@ -21,13 +21,13 @@ setInterval(() => {
         nav2.classList.add('active');
         about.style.display = "flex";
         homeStart.style.display = "none";
-        recoverPassword.style.display = "none";
+        supportService.style.display = "none";
     }
-    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#recoverPassword") {
+    else if (window.location == "https://shoffya.github.io/RI-Slaughterhouse/#supportService") {
         nav1.classList.remove('active');
         nav2.classList.remove('active');
         nav3.classList.add('active');
-        recoverPassword.style.display = "flex";
+        supportService.style.display = "flex";
         homeStart.style.display = "none";
         about.style.display = "none";
     }
@@ -37,6 +37,6 @@ setInterval(() => {
         nav1.classList.add('active');
         homeStart.style.display = "flex";
         about.style.display = "none";
-        recoverPassword.style.display = "none";
+        supportService.style.display = "none";
     }
 }, 500);
