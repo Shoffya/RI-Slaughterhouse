2c2487bc58ae271210e35fa96fd2fbb2b43cc102
Soffya <inguinha.fenandes@gmail.com>
2022-12-12 17:17:53 -0300

    modifying the layout of validatingForm

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -3,12 +3,11 @@ let btn = document.querySelector("#button");
 function validatingForm() {
     // var message = document.getElementById("formEmail").elements.namedItem("message").value;
     var message = document.querySelector(".message")
-    var tagA = document.querySelector(".button");
     var selectSubject = document.getElementById("subject");
     var opcaoTexto = selectSubject.options[select.selectedIndex].text;
 
     if (opcaoTexto == "Password Recovery") {
-        tagA.href = "mailto:turpyun.rigames@gmail.com?subject=" + opcaoTexto;
+        window.location.href = "mailto:turpyun.rigames@gmail.com?subject=" + opcaoTexto;
         console.log(tagA);
     }
 }
