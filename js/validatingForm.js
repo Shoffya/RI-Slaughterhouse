6d2bdeb93f98c4305c055f33bad28af26221208e
Soffya <inguinha.fenandes@gmail.com>
2022-12-13 13:43:11 -0300

    testing the form

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -1,13 +1,12 @@
 function validatingForm() {
     // var message = document.getElementById("formEmail").elements.namedItem("message").value;
+    var btnSubmit = document.querySelector(".btnSubmit");
     var subject = document.getElementById("subject");
     var opcaoTexto = subject.options[subject.selectedIndex].text;
     var opcaoValor = subject.options[subject.selectedIndex].value;
-    console.log(opcaoTexto); // Ferrari
-    console.log(opcaoValor); // ferrari
 
     if (opcaoTexto == "Password Recovery") {
-        // btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
+        btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
         console.log(opcaoTexto);
     }
 }
