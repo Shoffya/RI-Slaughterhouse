8a28ae25a84a9c2ae6f591b4d16643ed77666bac
Soffya <inguinha.fenandes@gmail.com>
2022-12-13 13:22:55 -0300

    testing the form

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -1,9 +1,10 @@
 function validatingForm() {
     // var message = document.getElementById("formEmail").elements.namedItem("message").value;
-    var message = document.querySelector(".message");
-    var btnSubmit = document.querySelector(".btnSubmit");
-    var selectSubject = document.getElementById("subject");
-    var opcaoTexto = selectSubject.options[select.selectedIndex].text;
+    var subject = document.getElementById("subject");
+    var opcaoTexto = subject.options[select.selectedIndex].text;
+    var opcaoValor = subject.options[select.selectedIndex].value;
+    console.log(opcaoTexto); // Ferrari
+    console.log(opcaoValor); // ferrari
 
     if (opcaoTexto == "Password Recovery") {
         // btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
