b07e0ba4288b7dbdd4aba47b6be31e43b6e1c6f6
Soffya <inguinha.fenandes@gmail.com>
2022-12-14 19:00:13 -0300

    testing the form

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -8,32 +8,15 @@ var opcaoTexto = subject.options[subject.selectedIndex].text;
 
 
 setInterval(() => {
-    for (var i = 0; i < subject.options.length; i++) {
-        if (subject.options[i].text === "Password Recovery") {
-            subject.selectedIndex = i;
-            console.log(subject.options[i].text);
-            break;
-        }
-        else if (subject.options[i].text === "Problems with a game") {
-            subject.selectedIndex = i;
-            console.log(subject.options[i].text);
-            break;
-        }
-        else if (subject.options[i].text === "Others") {
-            subject.selectedIndex = i;
-            console.log(subject.options[i].text);
-            break;
-        }
+    if (opcaoTexto == "Password Recovery") {
+        console.log(opcaoTexto);
     }
-    // if (opcaoTexto == "Password Recovery") {
-    //     console.log(opcaoTexto);
-    // }
-    // else if (opcaoTexto == "Problems with a game") {
-    //     console.log(opcaoTexto);
-    // }
-    // else if (opcaoTexto == "Others") {
-    //     console.log(opcaoTexto);
-    // }
+    else if (opcaoTexto == "Problems with a game") {
+        console.log(opcaoTexto);
+    }
+    else if (opcaoTexto == "Others") {
+        console.log(opcaoTexto);
+    }
 }, 500);
 
 
