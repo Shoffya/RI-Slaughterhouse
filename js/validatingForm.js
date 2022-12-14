4a2a2eec586ae4d319d5e81bc7ab2b4a2b6c39bd
Soffya <inguinha.fenandes@gmail.com>
2022-12-14 18:20:56 -0300

    testing the form

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -1,24 +1,28 @@
 var message = document.querySelector(".message");
 var messageTextarea = document.querySelector("#message");
+var btn = document.querySelector("#button");
 
-function validatingForm() {
-    var btnSubmit = document.querySelector(".btnSubmit");
-    var subject = document.getElementById("subject");
-    var opcaoTexto = subject.options[subject.selectedIndex].text;
+var btnSubmit = document.querySelector(".btnSubmit");
+var subject = document.getElementById("subject");
+var opcaoTexto = subject.options[subject.selectedIndex].text;
 
-    setInterval(() => {
-        if (opcaoTexto == "Password Recovery") {
-            message.style.display = "none";
-            btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
-        }
-        else if (opcaoTexto == "Problems with a game" || opcaoTexto == "Others") {
-            message.style.display = "flex";
-            if (messageTextarea.value == null) {
-                alert("Preencha por favor");
-            }
-            else {
-                btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}&body=${message.value}`;
-            }
-        }
-    }, 500)
-}
+
+setInterval(() => {
+    if (opcaoTexto == "Password Recovery") {
+        message.style.display = "none";
+        btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
+
+    }
+    else if (opcaoTexto == "Problems with a game" || opcaoTexto == "Others") {
+        // message.style.display = "flex";
+        // if (messageTextarea.value == "") {
+        //     alert("Preencha por favor");
+        // }
+        // else {
+        //     btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}&body=${message.value}`;
+        // }
+        console.log(messageTextarea.value);
+    }
+}, 500);
+
+
