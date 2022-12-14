d6e429c19e5a0648a68000653b222e1d21a9a5d4
Soffya <inguinha.fenandes@gmail.com>
2022-12-14 15:36:44 -0300

    testing the form

--- js/validatingForm.js
+++ js/validatingForm.js
@@ -6,17 +6,19 @@ function validatingForm() {
     var subject = document.getElementById("subject");
     var opcaoTexto = subject.options[subject.selectedIndex].text;
 
-    if (opcaoTexto == "Password Recovery") {
-        message.style.display = "none";
-        btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
-    }
-    else {
-        message.style.display = "flex";
-        if (message.value == null) {
-            alert("Preencha por favor");
+    setInterval(() => {
+        if (opcaoTexto == "Password Recovery") {
+            message.style.display = "none";
+            btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}`;
         }
         else {
-            btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}&body=${message.value}`;
+            message.style.display = "flex";
+            if (message.value == null) {
+                alert("Preencha por favor");
+            }
+            else {
+                btnSubmit.href = `mailto:turpyun.rigames@gmail.com?subject=${opcaoTexto}&body=${message.value}`;
+            }
         }
-    }
+    }, 500);
 }
